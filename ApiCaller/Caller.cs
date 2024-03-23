940384c2db674c8fa062e76b77fd7fb879a8b8ad
Louise Olsson <louise.m.k.olsson@hotmail.se>
2024-03-23 23:36:14 +0100

    Added API library
    class and logic for making api call

---
+++ ApiCaller/Caller.cs
@@ -0,0 +1,35 @@
+﻿namespace ApiCaller
+{
+    public class Caller
+    {
+        public async void TempCaller(string latitude, string longitude)
+        {
+            var result = await ApiPostCaller(latitude, longitude);
+
+            Console.WriteLine("Stop! Hammer time!");
+        }
+        private async Task<string> ApiPostCaller(string latitude, string longitude)
+        {
+            //StringContent stringContent = new();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+longitude+"/lat/"+latitude+"/data.json");
+
+                var postTask = client.PostAsync(client.BaseAddress, null);
+                postTask.Wait();
+
+                if (postTask.Result.IsSuccessStatusCode)
+                {
+                    var result = await postTask.Result.Content.ReadAsStringAsync();
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine(postTask.Result.ReasonPhrase);
+                    return null;
+                }
+            }
+        }
+    }
+}
--- FrostWarningSweden/WeatherCheck.cs
+++ FrostWarningSweden/WeatherCheck.cs
@@ -1,4 +1,5 @@
 using System;
+using ApiCaller;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +9,21 @@ namespace FrostWarningSweden
     public class WeatherCheck
     {
         [FunctionName("WeatherCheck")]
-        public void Run([TimerTrigger("0 0 18 * * * ")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            // 0 */1 * * * * - every minute - for testing purpose
+            // 0 0 18 * * * - at 18 every day
+            // Save our home position
+            string latitude = "57.844579";
+            string longitude = "11.896699";
+
+            Caller caller = new();
+            
+            caller.TempCaller(latitude, longitude);
+
+            // Give the push to pushover - how is this done?
         }
     }
 }
